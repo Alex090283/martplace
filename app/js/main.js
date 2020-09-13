85a11d51c830c5cda9b8edb9ff32e9bdbf1dd686
Alex090283 <alex.dev090283@gmail.com>
2020-09-13 17:10:51 +0300

    section slider end

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function() {
+
+
+    $('.slider__slide').slick({
+        arrows: true,
+        swipeToSlide: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '.arrow-left',
+        nextArrow: '.arrow-right',
+
+    });
+
+
+
+});
