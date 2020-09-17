5d89bf9a02da9d4345e14f23a8a6aa68e1d05214
Alex090283 <alex.dev090283@gmail.com>
2020-09-17 11:03:28 +0300

    feedback slider start

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,18 @@ $(function() {
 
     });
 
+    $('.feedback__items').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+            // swipeToSlide: true,
+            // infinite: true,
+            // cssEase: 'linear',
+            // prevArrow: '.arrow-left',
+            // nextArrow: '.arrow-right',
+
+    });
+
     var mixer = mixitup('.products__inner');
 
 
